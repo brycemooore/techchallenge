9608d22422f3d31edece354b00775a813e0b6211
brycemooore <brycemooore@gmail.com>
2021-02-23 13:33:51 -0500

    added tests

--- test.js
+++ test.js
@@ -5,4 +5,8 @@ describe("printNumAboveAndBelow", () => {
     it("when input is ([1, 5, 2, 1, 10], 6), output should be above: 1, below: 4", () => {
         expect(printNumsAboveAndBelow([1, 5, 2, 1, 10], 6)).to.equal("above: 1, below: 4");
     })
+
+    it("when input is ([20, 65, 678, 43, 106], 65), output should be above: 2, below: 2", () => {
+        expect(printNumsAboveAndBelow([20, 65, 678, 43, 106], 65)).to.equal("above: 2, below: 2");
+    })
 })
